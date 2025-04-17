d7c924b53d22495922d3f72385b1aabc23f28611
Ali747711 <126062847+Ali747711@users.noreply.github.com>
2025-04-17 18:23:02 +0900

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,15 +6,15 @@ export const API_URLS = {
   register: `${API_BASE_URL}/api/auth/register`,
   getUser: `${API_BASE_URL}/api/users/me`,
   updateProfile: `${API_BASE_URL}/api/users/update`,
-  getFriends: `${API_BASE_URL}/api/friends/`,
+  getFriends: `${API_BASE_URL}/api/friends`,
   searchUsers: `${API_BASE_URL}/api/users/search`,
-  sendFriendRequest: `${API_BASE_URL}/api/friends/request`,
+  sendFriendRequest: `${API_BASE_URL}/api/friends/request/:userId`,
   getReceivedRequests: `${API_BASE_URL}/api/friends/requests`,
   getSentRequests: `${API_BASE_URL}/api/friends/requests/sent`,
-  acceptFriendRequest: `${API_BASE_URL}/api/friends/request`,
-  rejectFriendRequest: `${API_BASE_URL}/api/friends/request`,
-  cancelFriendRequest: `${API_BASE_URL}/api/friends/request`,
-  removeFriend: `${API_BASE_URL}/api/friends`,
+  acceptFriendRequest: `${API_BASE_URL}/api/friends/request/:requestId/accept`,
+  rejectFriendRequest: `${API_BASE_URL}/api/friends/request/:requestId/reject`,
+  cancelFriendRequest: `${API_BASE_URL}/api/friends/request/:requestId`,
+  removeFriend: `${API_BASE_URL}/api/friends/:friendId`,
 };
 
 export default API_BASE_URL;
