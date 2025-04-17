1538b062cec9dde294110d1b5a40e2b579b601dd
Ali747711 <126062847+Ali747711@users.noreply.github.com>
2025-04-17 18:12:40 +0900

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,9 +6,15 @@ export const API_URLS = {
   register: `${API_BASE_URL}/api/auth/register`,
   getUser: `${API_BASE_URL}/api/users/me`,
   updateProfile: `${API_BASE_URL}/api/users/update`,
-  getFriends: `${API_BASE_URL}/api/friends/list`,
-  searchUsers: `${API_BASE_URL}/api/users/search`, // Add this line
-  // Add all other API endpoints
+  getFriends: `${API_BASE_URL}/api/friends/`,
+  searchUsers: `${API_BASE_URL}/api/users/search`,
+  sendFriendRequest: `${API_BASE_URL}/api/friends/request`,
+  getReceivedRequests: `${API_BASE_URL}/api/friends/requests`,
+  getSentRequests: `${API_BASE_URL}/api/friends/requests/sent`,
+  acceptFriendRequest: `${API_BASE_URL}/api/friends/request`,
+  rejectFriendRequest: `${API_BASE_URL}/api/friends/request`,
+  cancelFriendRequest: `${API_BASE_URL}/api/friends/request`,
+  removeFriend: `${API_BASE_URL}/api/friends`,
 };
 
 export default API_BASE_URL;
